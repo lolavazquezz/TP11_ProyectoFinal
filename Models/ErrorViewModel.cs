543dd3829cbe77d9c454fac366a81f87861435c0
LOLA VAZQUEZ <47436085@ort>
2023-11-07 08:21:01 -0300

    creando tp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP11_ProyectoFinal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
