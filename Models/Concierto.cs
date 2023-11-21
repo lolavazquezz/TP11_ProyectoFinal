b67a2195ac9b16701a00fa75b792b4b66abb19bd
AGUSTINA POTASMAN <47572491@ort>
2023-11-21 08:54:50 -0300

    Actualizado

--- Models/Concierto.cs
+++ Models/Concierto.cs
@@ -1,13 +1,23 @@
-public class Concierto{
-    public int IdConcierto{get; set;}
-    public string Nombre{get; set;}
-    public DateTime Fecha{get; set;}
-    public DateTime Hora{get; set;}
-    public int IdArtista{get; set;}
-    public string Ubicacion{get; set;}
-    public string Pais {get;set;} 
-    public string Imagen{get; set;}
-    public string Genero{get; set;}
-    public int Stock{get; set;}
-    public string Mes{get; set;}
+ public class Concierto
+{
+      public int IdConcierto{get; set;}
+      public string Nombre{get; set;}
+      public DateTime Fecha{get; set;}
+      public DateTime Hora{get; set;}
+      public int IdArtista{get; set;}
+     public string Ubicacion{get; set;}
+     public string Pais {get;set;} 
+     public string Imagen{get; set;}
+     public string Genero{get; set;}
+     public int Stock{get; set;}
+     public string Mes{get; set;}
+
+     public Concierto(){
+
+    }
+
+     public Concierto(string Nombre, DateTime Fecha,  DateTime Hora, int IdArtista,  string Ubicacion , string pais, string Imagen, string Genero, int Stock, String Mes){
+        
+    }
 }
+
