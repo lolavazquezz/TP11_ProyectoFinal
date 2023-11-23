9c2d17f3f6e13dd0a23dedf0a4f9c52ce401dbeb
Agus Potasman <128632108+AgustinaPotasman@users.noreply.github.com>
2023-11-23 16:54:57 -0300

    Actualizada bd

--- Models/BD.cs
+++ Models/BD.cs
@@ -1,53 +1,128 @@
-using System.Data.SqlClient;
+
+using System;
+using System.Collections.Generic;
 using Dapper;
-namespace TP11_ProyectoFinal.Models;
-public static class BD{
-    private static string ConnectionString { get; set; } = @"Server=localhost;DataBase=DB_Conciertos;Trusted_Connection=True;";
-    public static List<Concierto> traerConciertos()
+using System.Data.SqlClient;
+
+
+
+namespace TPFinal.Models
+{
+    public class BD
     {
-        List<Concierto> listaConciertos = new List<Concierto>();
-        using (SqlConnection db = new SqlConnection(ConnectionString))
+        private static string _connectionString = @"Server=localhost;DataBase=CuboGames;Trusted_Connection=True;";
+        private static List<concierto> listaconciertos = new List<concierto>();
+        private static List<Categoria> listaCategorias = new List<Categoria>();
+
+        public static List<concierto> Traerconciertos()
         {
-            string sql = "SELECT * FROM Concierto";
-            listaConciertos = db.Query<Concierto>(sql).ToList();
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * from conciertos";
+                listaconciertos = db.Query<concierto>(sql).ToList();
+            }
+            return listaconciertos;
         }
-        return listaConciertos;
-    }   
-     public static List<Concierto> traerConciertosIguales(string nombre)
-    {
-        List<Concierto> MisConciertos= new List<Concierto>();
-        using (SqlConnection db = new SqlConnection(ConnectionString))
+        public static List<Categoria> TraerCategorias()
         {
-            string sql = "SELECT * FROM Concierto WHERE Nombre=@pnombre";
-             MisConciertos =db.Query<Concierto>(sql, new { pnombre = nombre}).ToList();
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * from Categorias";
+                listaCategorias = db.Query<Categoria>(sql).ToList();
+            }
+            return listaCategorias;
         }
-        return MisConciertos;
-    }
-    public static string traerNombre(int IdConcierto)
-    {
-        string nombre="";
-        using (SqlConnection db = new SqlConnection(ConnectionString))
+        public static concierto verInfoconcierto(int idJ)
         {
-            string sql = "SELECT Nombre FROM Artista WHERE IdArtista = (SELECT IdArtista FROM Concierto WHERE IdArtista = @id)";
-            nombre = db.QueryFirstOrDefault<string>(sql, new { id = IdConcierto});
+            concierto conciertoActual = null;
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM conciertos WHERE Idconcierto = @pidconcierto";
+                conciertoActual = db.QueryFirstOrDefault<concierto>(sql, new { pidconcierto = idJ });
+            }
+            return conciertoActual;
         }
-        return nombre;
-    }
-    public static List<Concierto> traerInfo(int IdConcierto)
-    {
-        List<Concierto> listaInfo = new List<Concierto>();
-        using (SqlConnection db = new SqlConnection(ConnectionString))
+        public static void AgregarconciertoSP(concierto Jug)
         {
-            string sql = "SELECT * FROM Concierto WHERE IdConcierto = @id";
-            listaInfo = db.Query<Concierto>(sql, new { id = IdConcierto}).ToList();
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                db.Execute("sp_Agregarconcierto", new
+                {
+                    Nombre = Jug.Nombre,
+                    CantLikes = Jug.CantLikes,
+                    Descripcion = Jug.Descripcion,
+                    FechaCreacion = Jug.FechaCreacion,
+                    Imagen = Jug.Imagen,
+                    Precio = Jug.Precio,
+                    fkCategoria = Jug.fkCategoria
+                }, commandType: CommandType.StoredProcedure);
+            }
         }
-        return listaInfo;
-    }
-   
-    public static void InsertarCompra(string email, int idConcierto, int cantidad){
-        string SQL = "INSERT INTO Compra(Email, IdConcierto, Cantidad) VALUES (@pEmail, @pIdConcierto, @pCantidad)";
-        using(SqlConnection db = new SqlConnection(ConnectionString)){
-            db.Execute(SQL, new {pEmail = email, pIdConcierto = idConcierto, pCantidad = cantidad});
+        public static Tarjeta VerificarSiExisteTarjeta(int numero)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Tarjeta WHERE Numero = @pNumero";
+                return db.QueryFirstOrDefault<Tarjeta>(sql, new { pNumero = numero});
+            }
+        }   
+        public static int ActualizarLikesconciertoSP(int idconcierto, int cantLikes)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string idconciertoStr = idconcierto.ToString();
+                return db.Execute("sp_ActualizarLikesconcierto", new
+                {
+                    Idconcierto = idconciertoStr,
+                    CantLikes = cantLikes
+                }, commandType: CommandType.StoredProcedure);
+            }
         }
+        public static int VerCantLikes(int idJ)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT CantLikes FROM conciertos WHERE Idconcierto = @pIdconcierto";
+                return db.QueryFirstOrDefault<int>(sql, new { pIdconcierto = idJ });
+            }
+        }
+
+        public static int AgregarUsuarioSP(Usuario usuario)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                return db.Execute("sp_AgregarUsuario", new
+                {
+                    Contraseña = usuario.Contraseña,
+                    Nombre = usuario.Nombre
+                }, commandType: CommandType.StoredProcedure);
+            }
+        }
+        public static Usuario BuscarUsuario(Usuario U)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Usuarios WHERE IdUsuario = @U.IdUsuario";
+                return db.QueryFirstOrDefault<Usuario>(sql);
+            }
+        }
+        public static Usuario BuscarUsuarioXNombre(string nombre)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Usuarios WHERE Nombre = @Nombre";
+                return db.QueryFirstOrDefault<Usuario>(sql, new{ Nombre = nombre });
+            }
+        }
+        public static Usuario BuscarContraXUsuario(string nombre)
+        {
+            using (SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT Contraseña FROM Usuarios WHERE Nombre = @Nombre";
+                return db.QueryFirstOrDefault<Usuario>(sql, new{ Nombre = nombre });
+            }
+        }
+
+
     }
 }
